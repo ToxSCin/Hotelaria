ef29281f68136821e1e97e4b4137c72e318139ca
Usuario <Usuario@CPD03-M14>
2022-09-22 14:36:39 -0300

    Adicionar arquivos de projeto.

---
+++ HotelariaBetter/HotelariaBetter/Model/Suite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelariaBetter.Model
+{
+    internal class Suite
+    { 
+
+
+
+
+         
+        
+              public string Descricao { get; set; }
+        public double ValorDiariaAdulto { get; set; }
+        public double ValorDiariaCrianca { get; set; }
+    }
+}
